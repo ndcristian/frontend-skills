27e60efd7a9720771d673b5713916eda5965dd10
Cristian Nicolae <cristian.nicolae@quest-global.com>
2024-07-17 18:09:05 +0300

    generator functions

--- non_angular/plain_js/async await/index.js
+++ non_angular/plain_js/async await/index.js
@@ -21,33 +21,3 @@ setTimeout(() => {
 }, 3000);
 
 
-// Array
-const colors = ["red", "green", "blue"];
-// Set
-const uniqueColors = new Set(colors);
-// Generator
-function* generateNumbers() {
-  yield 1;
-  yield 2;
-  yield 3;
-}
-
-const numbersGenerator = generateNumbers();
-console.log([...numbersGenerator])
-
-function* foo(index) {
-    while (index < 100) {
-      yield index;
-      index++;
-    }
-  }
-  
-  const iterator = foo(0);
-  
-  for(let i=0; i<100; i++){
-    console.log(iterator.next().value);
-  }
-  console.log(iterator.next().value);
-  // Expected output: 0
-  
-//   console.log(iterator.next().value);
