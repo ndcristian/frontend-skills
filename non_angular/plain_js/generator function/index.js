f86e0e2ef0cf9c039cf3725ce066c4434d3fa77c
Cristian Nicolae <cristian.nicolae@quest-global.com>
2024-07-18 09:38:41 +0300

    javascript symbol

--- non_angular/plain_js/generator function/index.js
+++ non_angular/plain_js/generator function/index.js
@@ -10,21 +10,24 @@ function* generateNumbers() {
 }
 
 const numbersGenerator = generateNumbers();
-console.log([...numbersGenerator])
+console.log([...numbersGenerator]);
 
 function* foo(index) {
-    while (index < 100) {
-      yield index;
-      index++;
-    }
+  while (index < 100) {
+    yield index;
+    index++;
   }
-  
-  const iterator = foo(0);
-  
-  for(let i=0; i<100; i++){
-    console.log(iterator.next().value);
-  }
-  console.log(iterator.next().value);
-  // Expected output: 0
-  
+}
+
+const iterator = foo(0);
+// wil display all generated number as un array
+console.log("IT::",[...iterator]); // [0,1,2,3,......100]
+for (let i = 0; i < 100; i++) {
+  console.log(iterator.next().value); // if line 24 is not comment this wil display undefined 100 times
+}
+console.log(iterator.next().value);//if line 24 is not comment this wil display undefined 
+console.log(iterator.next().value);//if line 24 is not comment this wil display undefined 
+console.log(iterator.next().value);//if line 24 is not comment this wil display undefined 
+// Expected output: 0
+
 //   console.log(iterator.next().value);
---
+++ non_angular/plain_js/symbol/index.js
@@ -0,0 +1,26 @@
+// A “symbol” represents a unique identifier.
+// A value of this type can be created using Symbol():
+// Symbols are guaranteed to be unique. Even if we create many symbols with exactly the same description, 
+// they are different values. The description is just a label that doesn’t affect anything
+
+let id = Symbol("id");
+let user = {
+  name: "John",
+  age: 30,
+  [id]: 123
+};
+
+for (let key in user) console.log(key); // name, age (no symbols)
+
+// the direct access by the symbol works
+console.log( "Direct: " + user[id] );
+
+let user2 = { // belongs to another code
+    name: "John"
+  };
+  
+  let id2 = Symbol("id2");
+  
+  user2[id2] = 1;
+  
+  console.log( user2[id2] ); 
